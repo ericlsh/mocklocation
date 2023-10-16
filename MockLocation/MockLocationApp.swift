ceb84b8f10de3039f0021a332e469e50a0d5abec
Eric Leung <ericleungszeho.ca@gmail.com>
2023-10-16 10:04:24 -0700

    Initial Commit

---
+++ MockLocation/MockLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MockLocationApp.swift
+//  MockLocation
+//
+//  Created by Eric Leung on 2023-10-16.
+//
+
+import SwiftUI
+
+@main
+struct MockLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
